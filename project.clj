706b688dbc2b44b057c0014390e00b21a26c57bd
Austin Haas <austin@pettomato.com>
2016-08-26 17:00:20 -0700

    project.clj: bump clojure

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://pettomato.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [;;[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.9.0-alpha11"]
                  [org.clojure/core.async "0.2.385"]
                  [pt-lib "0.1.0-SNAPSHOT"]])
