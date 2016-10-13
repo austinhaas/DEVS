51ab8247b5fa28beb21309e9c79ec6981e51f689
Austin Haas <austin@pettomato.com>
2016-10-12 18:12:30 -0700

    aF-real-time-system: Add max-delta parameter.

--- src/pettomato/devs/aF_real_time_system.cljs
+++ src/pettomato/devs/aF_real_time_system.cljs
@@ -5,18 +5,20 @@
    [pettomato.devs.immediate-system :refer [immediate-step]]))
 
 (defn aF-real-time-system-start!
-  ([sim start-time input] (aF-real-time-system-start! sim start-time input nil))
-  ([sim start-time input output]
-   (let [aF (atom nil)
-         wc-start-time (now)]
-     (letfn [(step [sim t]
-               (let [sim-t (+ start-time (- t wc-start-time))
-                     ev*   (get-and-set! input [])
-                     tmsg* (map (fn [m] [(dec sim-t) m]) ev*)
-                     [sim' out] (immediate-step sim (tl sim) sim-t tmsg*)]
-                 (when (and output (seq out)) (swap! output conj [sim-t out]))
-                 (reset! aF (js/requestAnimationFrame (fn [t] (step sim' t))))))]
-       (step (init sim start-time) (now))
+  ([sim start-time max-delta input]
+   (aF-real-time-system-start! sim start-time max-delta input nil))
+  ([sim start-time max-delta input output]
+   (let [aF (atom nil)]
+     (letfn [(step [sim sim-t t t']
+               (let [delta      (- t' t)
+                     delta'     (min delta max-delta)
+                     sim-t'     (+ sim-t delta')
+                     ev*        (get-and-set! input [])
+                     tmsg*      (map (fn [m] [(dec sim-t') m]) ev*)
+                     [sim' out] (immediate-step sim (tl sim) sim-t' tmsg*)]
+                 (when (and output (seq out)) (swap! output conj [sim-t' out]))
+                 (reset! aF (js/requestAnimationFrame (fn [t] (step sim' sim-t' t' t))))))]
+       (step (init sim start-time) start-time (now) (now))
        aF))))
 
 (defn aF-real-time-system-stop! [handle]
