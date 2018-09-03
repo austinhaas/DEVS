65e06fa501d082103b9bbd57e1e05887e6b8a34b
Austin Haas <austin@pettomato.com>
2018-09-03 12:00:25 -0700

    test: Cleanup

--- test/pettomato/devs/test.clj
+++ test/pettomato/devs/test.clj
@@ -1,8 +1,7 @@
 (ns pettomato.devs.test
   (:require
    [clojure.test :refer :all]
-   [pettomato.devs.util :refer [dissoc-in]]
-   [pettomato.devs.util :refer [infinity]]
+   [pettomato.devs.util :refer [dissoc-in infinity]]
    [pettomato.devs.test-util :refer [eq?]]
    [pettomato.devs.models :refer [atomic-model executive-model network-model register unregister connect disconnect]]
    [pettomato.devs.atomic-simulator :refer [atomic-simulator]]
