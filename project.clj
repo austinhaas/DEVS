e2ea23bd9c5eef83819c7e93a5d612c05a604423
Austin Haas <austin@pettomato.com>
2020-06-30 19:23:44 -0700

    Delete project.clj

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject pettomato/devs "0.1.0-SNAPSHOT"
-  :description "A discrete-event simulation system based on the DEVS formalism."
-  :url "http://pettomato.com"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [pettomato/lib "0.1.0-SNAPSHOT"]])
